17464dc601602dc7f7bcb3eece318dec7f80090e
Garren Smith <garren.smith@gmail.com>
2012-05-05 10:25:55 +0200

    update slides

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/comment',function (req, res) {
 
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
